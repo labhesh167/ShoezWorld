256d5f1bd8f950fc8fa0e7d24d031261727a3988
Labhesh Chaudhari <78224580+labhesh167@users.noreply.github.com>
2025-06-02 15:59:43 +0530

    Delete ShoezWorld directory

--- ShoezWorld/src/main/java/com/example/ShoezWorld/Repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ShoezWorld.Repository;
-
-import com.example.ShoezWorld.Model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
